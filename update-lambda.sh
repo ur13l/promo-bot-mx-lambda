36b68b183ae04830f8d7c66c150348c418174d33
Edgar Perez <edgarp.dev@gmail.com>
2020-07-24 20:28:57 -0500

    Removing unnecessary files

--- update-lambda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# WARNING: This profile must change to match the custom named profile assigned on your ~/.aws/credentials and ~/.aws/config
-# For more information: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html
-export AWS_PROFILE=${2:-uriel}
-echo "/usr/local/bin/aws lambda update-function-code $1 --function-name $1 --zip-file fileb://$1.zip";
-/usr/bin/zip $1/$1.zip $1
-/usr/local/bin/aws lambda update-function-code --function-name $1 --zip-file fileb://$1/$1.zip
